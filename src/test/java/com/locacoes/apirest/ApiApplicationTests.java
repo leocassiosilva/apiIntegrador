da155c058f5697c05930331a1c335adea1c286d1
leocassiosilva <leocassiosilva1234@gmail.com>
2020-12-09 21:29:23 -0300

    first commit

---
+++ src/test/java/com/locacoes/apirest/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locacoes.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
