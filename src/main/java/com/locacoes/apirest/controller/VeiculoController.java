e5b80e6ace9a8971723a1d1a952a8c7cd05ec8cc
Felipe Gabriel <fgabrielqr@gmail.com>
2020-12-30 23:04:27 -0300

    apiBuscar

--- src/main/java/com/locacoes/apirest/controller/VeiculoController.java
+++ src/main/java/com/locacoes/apirest/controller/VeiculoController.java
@@ -1,12 +1,17 @@
 package com.locacoes.apirest.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.locacoes.apirest.repository.VeiculoRepository;
@@ -27,6 +32,17 @@ public class VeiculoController {
 	@GetMapping("/veiculos/{id}")
 	public Optional<Veiculo> listaveiculoUnico(@PathVariable("id") Long id) {
 		return veiculoRepository.findById(id);
+	}
+	
+	@GetMapping("/buscar")
+	public ResponseEntity<List<Veiculo>> buscarVeiculos(
+			@RequestParam("dataRetirar") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataRetirar,
+			@RequestParam("dataDevolver") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataDevolver,
+			@RequestParam("nome") String nome){
+		
+		List<Veiculo> veiculo = veiculoRepository.buscarVeiculos(dataRetirar, dataDevolver, nome);
+		return new ResponseEntity<List<Veiculo>>(veiculo, HttpStatus.OK);
+		
 	}
 	
 }
--- src/main/java/com/locacoes/apirest/repository/VeiculoRepository.java
+++ src/main/java/com/locacoes/apirest/repository/VeiculoRepository.java
@@ -1,5 +1,7 @@
 package com.locacoes.apirest.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,4 +20,17 @@ public interface VeiculoRepository extends JpaRepository<Veiculo, Long>{
 			+ "inner join marca on(tipomarca.id_marca = marca.id_marca)\n"
 			+ "inner join locadora on(v.id_locadora = locadora.id_locadora)\n" + "where v.id_veiculo = :id", nativeQuery = true)
 	Veiculo buscarVeiculo(@Param("id") Long id);
+
+	@Query(name = "buscarPorDatas", value = "SELECT * FROM veiculos as v "
+			+ "inner join locadora on (locadora.id_locadora = v.id_locadora) "
+			+ "inner join categoria on(categoria.id_categoria = v.id_categoria)"
+			+ "inner join tipomarca on (tipomarca.id_tipo_marca = v.id_tipo_marca)\r\n"
+			+ "inner join marca on (marca.id_marca = tipomarca.id_marca)\r\n"
+			+ "inner join tipo on (tipo.id_tipo = tipomarca.id_tipo)"
+			+ "inner join local on (local.id_local = locadora.id_local)"
+			+ "where v.id_veiculo NOT IN (SELECT locacao.id_veiculo FROM locacao "
+			+ "where (locacao.data_retirada <= :retirada and locacao.data_retirada >= :retirada) "
+			+ "or (locacao.data_entrega >= :devolucao and locacao.data_entrega <= :devolucao)) and local.nome = :nome", nativeQuery = true)
+	List<Veiculo> buscarVeiculos(LocalDate retirada, LocalDate devolucao, String nome);
+
 }
