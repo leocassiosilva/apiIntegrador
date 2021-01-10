27358c98ee07285221895bd4c4a5da677097238a
leocassiosilva <leocassiosilva1234@gmail.com>
2021-01-10 12:23:43 -0300

    corrigindo

--- src/main/java/com/locacoes/apirest/controller/LocacoesController.java
+++ src/main/java/com/locacoes/apirest/controller/LocacoesController.java
@@ -3,9 +3,12 @@ package com.locacoes.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +25,13 @@ public class LocacoesController {
 	@Autowired
 	LocacoesRepository locacoesRepository;
 
-	@PostMapping("/confirmar")
+	@PostMapping("/locacoes/confirmar")
 	@ApiOperation(value = "Retorna uma locação salva")
-	public Locacao salvarLocacao(@RequestParam("locacao") Locacao locacao) {
-		return locacoesRepository.save(locacao);
+	public ResponseEntity<Locacao> salvarLocacao(@RequestBody Locacao locacao) {
+		locacoesRepository.save(locacao);
+		return new ResponseEntity<Locacao>(locacao, HttpStatus.OK);
 	}
+	
 
 	@GetMapping("/locacoes/{email}")
 	@ApiOperation(value = "Lista as locações de um usuario")
